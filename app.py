54a4af9740c627a7f6e37015143977084b843aa9
abkamand <56053737+abkamand@users.noreply.github.com>
2021-08-10 15:31:10 -0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, json, jsonify, request, render_template
+import requests
+
+
+app = Flask(__name__)
+
+
+# transform youtube video id into a youtube embed link and return
+@app.route("/embedlink")
+def embed_link():
+    video_id = request.args.get('videoid')
+    embed_link = "https://www.youtube.com/embed/" + video_id
+
+    return embed_link
+
+
+# Listener
+if __name__ == "__main__":
+    #Start the app on port 3000, it will be different once hosted
+    app.run(port=65334, debug=True)
